2d60d4fd9cba71b81375ce15bbf46ee8b7d1932a
Guido Lange <32455006+Gylxan@users.noreply.github.com>
2020-01-25 12:32:00 +0100

    First implementation to get a random game

---
+++ src/interfaces/Game.ts
@@ -0,0 +1,31 @@
+export interface Game {
+  name: string;
+  url: string;
+}
+
+export const GAMES: Array<Game> = [
+  {
+    name: 'Game 1',
+    url: 'game1',
+  },
+  {
+    name: 'Rätselraten',
+    url: 'puzzle',
+  },
+  {
+    name: 'Pantomime',
+    url: 'pantomime',
+  },
+  {
+    name: 'Trinken',
+    url: 'drink',
+  },
+  {
+    name: 'Musik',
+    url: 'music',
+  },
+  {
+    name: 'What?!?!',
+    url: 'what',
+  },
+];
--- src/pages/games/player/Overview.tsx
+++ src/pages/games/player/Overview.tsx
@@ -1,18 +1,87 @@
-import React from 'react';
-import logo from '../../../assets/images/logo.png';
-import styles from '../../Home/Home.module.css';
+import React, { CSSProperties } from 'react';
+import styles from './Overview.module.css';
+import { Game, GAMES } from '../../../interfaces/Game';
+import { generateColor } from '../../../services/color';
+import { Button } from '@material-ui/core';
+import { getRandomItem } from '../../../services/array';
 
-const Overview: React.FC = () => (
-  <div className={styles.Home}>
-    PLAYER
-    <img src={logo} className={styles.Logo} alt="logo" />
-    <p>
-      Edit <code>src/App.js</code> and save to reload.
-    </p>
-    <a className={styles.Link} href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-      Learn React
-    </a>
-  </div>
-);
+export interface State {
+  activeGame: string | null;
+  coloredGames: Array<GameWithColor>;
+}
+
+export interface GameWithColor extends Game {
+  color: string;
+}
+class Overview extends React.PureComponent<{}, State> {
+  state = {
+    activeGame: null,
+    coloredGames: GAMES.map((game: Game) => {
+      return { ...game, color: generateColor() };
+    }),
+  };
+  interval: undefined | number = undefined;
+  timeout: undefined | number = undefined;
+
+  endTimeout = (): void => {
+    if (this.timeout) {
+      window.clearTimeout(this.timeout);
+    }
+  };
+  startRandomGameChoose = (): void => {
+    this.endTimeout();
+    this.timeout = window.setTimeout(() => {
+      this.startInterval();
+    }, 0);
+  };
+
+  stopRandomGameChoose = (): void => {
+    this.endTimeout();
+    this.endInterval();
+  };
+
+  endInterval = (): void => {
+    if (!!this.interval) {
+      window.clearInterval(this.interval);
+    }
+  };
+
+  startInterval = (): void => {
+    this.interval = window.setInterval(() => {
+      this.setState({
+        activeGame: getRandomItem(this.state.coloredGames).name,
+      });
+    }, 500);
+  };
+
+  render(): JSX.Element {
+    return (
+      <div className={styles.Container}>
+        <div className={styles.Games}>
+          {this.state.coloredGames.map((game: GameWithColor) => {
+            const css: CSSProperties = { backgroundColor: game.color };
+            if (game.name === this.state.activeGame) {
+              css.boxShadow = `0 0 10px 10px rgba(255,255,255,0.5)`;
+            }
+            return (
+              <div key={game.name} className={styles.Tile} style={css}>
+                {game.name}
+              </div>
+            );
+          })}
+        </div>
+
+        <Button
+          className={styles.StartButton}
+          variant={'contained'}
+          color={'primary'}
+          onClick={!this.timeout ? this.startRandomGameChoose : this.stopRandomGameChoose}
+        >
+          {!!this.timeout ? 'Stop' : 'Spiel auswählen!'}
+        </Button>
+      </div>
+    );
+  }
+}
 
 export default Overview;
