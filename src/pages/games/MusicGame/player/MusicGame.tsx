27891cdf38d28f86a1fae0417142dec655bdcbff
Kristina Mergenthaler <kristina@20steps.de>
2020-01-25 16:33:31 +0100

    added description for music game

--- src/pages/games/MusicGame/player/MusicGame.tsx
+++ src/pages/games/MusicGame/player/MusicGame.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import styles from './MusicGame.module.css';
+import { Button } from '@material-ui/core';
 
 export interface Props {}
 
@@ -9,7 +11,16 @@ class MusicGame extends React.PureComponent<Props, State> {
     return (
       <>
         <h1>Musik-Quiz</h1>
-        <p></p>
+        <div className={styles.description}>
+          <p><b>Teilnehmer:</b> alle</p>
+          <p><b>Modus:</b> abwechselnd</p>
+          <p>
+            <b>Beschreibung:</b> Den Teams werden abwechselnd die ersten 10 Sekunden eines Hits der 70er/80er/90er vorgespielt.
+            Das Team hat 30 Sekunden um eine Antwort zu geben.<br></br>
+            Es werden 8 Runden gespielt.
+          </p>
+        </div>
+        <Button variant={'contained'} color={'primary'}>Okay</Button>
       </>
     );
   }
