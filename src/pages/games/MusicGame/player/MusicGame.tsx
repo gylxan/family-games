2dcfb19ff705b38a74615ccf88c90a4a2cf71f03
Guido Lange <32455006+Gylxan@users.noreply.github.com>
2020-01-26 15:27:35 +0100

    CRLF to LF

--- src/pages/games/MusicGame/player/MusicGame.tsx
+++ src/pages/games/MusicGame/player/MusicGame.tsx
@@ -29,8 +29,8 @@ class MusicGame extends React.PureComponent<Props, State> {
               <strong>Modus:</strong> Battle
             </p>
             <p>
-              <strong>Beschreibung:</strong> Den Teams werden abwechselnd die ersten 10 Sekunden eines Hits der
-              70er/80er/90er vorgespielt. Das Team hat 30 Sekunden um eine Antwort zu geben.
+              <strong>Beschreibung:</strong> Den Teams werden abwechselnd die ersten 10 Sekunden eines Hits der 70er /
+              80er / 90er vorgespielt. Das Team hat 30 Sekunden um eine Antwort zu geben.
               <br />
               Es werden 8 Runden gespielt.
             </p>
