36e966f55933ca04f3802958b5f152d2b3b414b9
Guido Lange <32455006+Gylxan@users.noreply.github.com>
2020-02-03 12:53:17 +0100

    Added animations and intro theme

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,13 +4,26 @@ import styles from './Home.module.css';
 import { Button } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 import { LinkTo, Routes } from '../../services/routes';
+// @ts-ignore
+import looneyTunesIntro from '../../assets/audio/looney_tunes_intro.mp3';
+import classNames from 'classnames';
 
 const Home: React.FC = () => {
   const [clickCounter, setClickCounter] = useState(0);
   return (
     <div className={styles.Home}>
-      <h1>Familien-Quiz</h1>
-      <img src={logo} className={styles.Logo} alt="logo" />
+      <div className={classNames(styles.Title, 'animated', 'rubberBand', 'slow', 'delay-5s')}>
+        <h1 className={classNames('animated', 'bounceInDown', 'slow')}>Fa</h1>
+        <h1 className={classNames('animated', 'bounceInDown', 'slow', 'delay-1s')}>mi</h1>
+        <h1 className={classNames('animated', 'bounceInDown', 'slow', 'delay-2s')}>lien</h1>
+        <h1 className={classNames('animated', 'bounceInDown', 'slow', 'delay-3s')}>-</h1>
+        <h1 className={classNames('animated', 'bounceInDown', 'slow', 'delay-4s')}>Quiz</h1>
+      </div>
+      <img
+        src={logo}
+        className={classNames(styles.Logo, 'animated', 'jackInTheBox', 'slower', 'delay-5s')}
+        alt="logo"
+      />
       <div className={styles.Control}>
         <Link to={LinkTo.teamPreparation(1)}>
           <Button className={styles.StartButton} variant={'contained'} color={'primary'}>
@@ -24,6 +37,7 @@ const Home: React.FC = () => {
         >
           <Button>Ich bin Master!</Button>
         </Link>
+        <audio src={looneyTunesIntro} autoPlay />
       </div>
     </div>
   );
