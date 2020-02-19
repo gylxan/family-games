8dd30d3972a2c725cc2eb32b85c8ad5299ed6cd5
gylxan <guidolange90@gmail.com>
2020-02-19 20:57:15 +0100

    Added master overview route

--- src/services/routes.ts
+++ src/services/routes.ts
@@ -10,6 +10,7 @@ export enum Routes {
 export const LinkTo = {
   playerGame: (game: string): string => `${Routes.Player}${Routes.Games}${game}`,
   playerGamesOverview: (): string => `${Routes.Player}${Routes.Games}`,
+  masterGamesOverview: (): string => `${Routes.Master}${Routes.Games}`,
   masterGame: (game: string): string => `${Routes.Master}${Routes.Games}${game}`,
   teamPreparation: (id: number): string => Routes.TeamPreparation.replace(':id', '' + id),
 };
