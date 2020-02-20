a5d43a24bdf60fe9ffde7f0964229b9631b7cddf
gylxan <guidolange90@gmail.com>
2020-02-20 20:31:44 +0100

    Style Blur

--- src/pages/games/Blur/Blur.tsx
+++ src/pages/games/Blur/Blur.tsx
@@ -114,16 +114,19 @@ class Blur extends React.PureComponent<Props, State> {
       return <LinearProgress />;
     }
     return (
-      <GameFlow
-        rounds={MAX_ROUNDS}
-        countdown={60}
-        showScoring
-        showRoundIndicator={false}
-        descriptionComponent={this.renderGameDescription()}
-        playingComponent={this.renderGame()}
-        onStartPlaying={this.startNextTurn}
-        onCountdown={this.handleCountdown}
-      />
+      <>
+        <h1 className={styles.Title}>Wischi-Waschi</h1>
+        <GameFlow
+          rounds={MAX_ROUNDS}
+          countdown={60}
+          showScoring
+          showRoundIndicator={false}
+          descriptionComponent={this.renderGameDescription()}
+          playingComponent={this.renderGame()}
+          onStartPlaying={this.startNextTurn}
+          onCountdown={this.handleCountdown}
+        />
+      </>
     );
   }
 }
